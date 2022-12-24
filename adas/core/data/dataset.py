f77e98f6cb4e827b0a44d7a21adf19395da26a2a
Pupy101 <ser.porkhun41@gmail.com>
2022-12-24 06:16:50 +0300

    Fix mixing class

--- adas/core/data/dataset.py
+++ adas/core/data/dataset.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List, Optional, Set, Union
+from typing import List, Set, Union
 
 import numpy as np
 from PIL import Image
@@ -14,9 +14,6 @@ class MixingDataset:
         return np.array(Image.open(path))
 
     @staticmethod
-    def find_files(
-        directory: Union[str, Path], extensions: Optional[Set[str]] = None
-    ) -> List[Path]:
+    def find_files(directory: Union[str, Path], extensions: Set[str]) -> List[Path]:
         """Find all files with setted extension"""
-        extensions = extensions or {".png", ".jpg", ".jpeg"}
-        return [file for file in Path(directory).rglob("*") if file.suffix in extensions]
+        return [file for file in Path(directory).rglob("*") if file.suffix.lower() in extensions]
